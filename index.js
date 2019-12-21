12f66a6eda7dc532dd3d47b80004a6db441f041a
Neal JIn Oh <no4259@gmail.com>
2019-12-21 02:02:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
-/* Given Code, don't edit */
-
-function handleClick(e) {
-  let greet = document.getElementById('time').value
-  displayMessage(greet(timeString))
+function greet(){
+  if(){
+    
+  }
 }
-
-/* End Given Code, don't edit above here...*/
-
-/* Write your implementation of greet() */
-/* Write your implementation of displayMessage() */
